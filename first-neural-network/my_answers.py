00d3f03a65ed3474ef7ffdb10b2f5ecb1ab1c609
jetafull <jetafull@gmail.com>
2018-01-28 12:23:37 +0900

    1st project: passed ua

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -92,16 +92,16 @@ class NeuralNetwork(object):
         output_error_term = error 
         
         # TODO: Calculate the hidden layer's contribution to the erro
-        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T)
+        hidden_error = np.dot(self.weights_hidden_to_output, output_error_term)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
         #output_error_term = None
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs) 
         
         # Weight step (input to hidden)
-        delta_weights_i_h += self.lr * hidden_error_term * X[:, None] 
+        delta_weights_i_h +=  hidden_error_term * X[:, None] 
         # Weight step (hidden to output)
-        delta_weights_h_o += (self.lr * output_error_term * hidden_outputs)[:, None]
+        delta_weights_h_o += (output_error_term * hidden_outputs)[:, None]
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -114,8 +114,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += delta_weights_i_h # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 400
-learning_rate = 0.01
-hidden_nodes = 3 
+iterations = 3000
+learning_rate = 0.7
+hidden_nodes = 10 
 output_nodes = 1
